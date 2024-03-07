d0dd665106b91c7dfbf8d34fbd460ebc31412f81
Kunal Jadhav <154059626+Itsbitscoder@users.noreply.github.com>
2024-03-07 15:23:17 +0530

    script.js added

---
+++ script.js
@@ -0,0 +1,22 @@
+const slider = document.querySelector('.slider');
+
+// function activate(e) {
+//   const items = document.querySelectorAll('.item');
+//   e.target.matches('.next') && slider.append(items[0]);
+//   e.target.matches('.prev') && slider.append(items[items.length - 1]);
+
+
+// }
+// document.addEventListener('click',activate,false);
+function activate(e) {
+    const items = document.querySelectorAll('.item');
+    const slider = document.querySelector('.slider');
+    if (e.target.matches('.next')) {
+      slider.appendChild(items[0]);
+    } else if (e.target.matches('.prev')) {
+      slider.prepend(items[items.length - 1]);
+    }
+  }
+
+  document.addEventListener('click', activate, false);
+  
